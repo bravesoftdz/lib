cc1f40b7962be0e3d8fd208358a13c6c97059c0f
nabbla [work] <nabbla@yandex.ru>
2016-03-03 20:12:13 +0400

    I want to refactor some of my most important code, make it less dependent, so unused parts could be left out. Seems right way to do, hope not to destroy it completely.

--- streaming_class_lib.pas
+++ streaming_class_lib.pas
@@ -1,77 +1,94 @@
 unit streaming_class_lib;
 
+(*
+    This unit makes work with Delphi streaming system much easier.
+
+    TStreamingClass is defined which should be base class instead of TComponent,
+    in case you want to easily save its contents to file/stream/string or
+    load them back. It defines easiest child/parent relation needed for streaming
+    system to work, that is: Parent is same as owner, children are owner's components.
+
+    *)
+
 interface
 uses Classes,sysutils,typInfo;
 
 type
 
-StreamingClassSaveFormat=(fBinary,fAscii,fCyr);
-
-TSaveToFileProc = procedure(const filename: string) of object;
+TStreamingClassSaveFormat=Integer;
 
 TstreamingClass=class(TComponent)
-  private
-    procedure RecursiveEnsure(our_root,aowner: TStreamingClass);
   protected
     procedure   GetChildren(Proc: TGetChildProc; Root: TComponent); override;
     function    GetChildOwner: TComponent; override;
-    procedure myGetPropInfo(propPath: string; out instance: TPersistent; out fPropInfo: PPropInfo);
-    function OwnedBy(Component, Owner: TComponent): Boolean;
   public
-    saveFormat: StreamingClassSaveFormat;
-    function GetComponentValue(Component,LookUpRoot: TComponent): string;
-    constructor LoadFromFile(const filename: string); virtual;  //неужели до меня дошло?
+    saveFormat: TStreamingClassSaveFormat;
+    //these constructors are easiest to use, but you should already know what class
+    //you're expecting to get.
+    //If you don't know which class you're loading, use class functions LoadComponent,
+    //and cast them accordingly.
+    constructor LoadFromStream(stream: TStream); virtual;//yup, it must be constructor,
+    constructor LoadFromFile(const filename: string); //not a function or class function
     constructor LoadFromString(const text: string);
-    constructor Clone(source: TStreamingClass;owner: TComponent=nil);
-    //конструктор это конечно хорошо, однако нужно знать, какой класс имеет созд. объект
-    //опробуем классовые функции
+
+    class function LoadComponentFromStream(stream: TStream): TComponent;
     class function LoadComponentFromString(const text: string): TComponent;
     class function LoadComponentFromFile(const FileName: string): TComponent;
-    class function CloneComponent(source: TStreamingClass; aowner: TComponent=nil): TComponent;
+
     procedure Clear; virtual;
-    //будет вызываться перед Assign, чтобы инициализировать объект нач. значениями
-    procedure SaveToFile(const filename: string); virtual;
+    procedure SetDefaultProperties;
+    procedure Assign(source: TPersistent); override;
+    constructor Clone(source: TStreamingClass; owner: TComponent=nil);
+    class function CloneComponent(source: TStreamingClass; aowner: TComponent=nil): TComponent;
+
+    procedure SaveToStream(stream: TStream); virtual;
+    procedure SaveToFile(const filename: string);
+    function SaveToString: string;
+
     procedure SaveBinaryToFile(const filename: string);
     procedure LoadBinaryFromFile(const filename: string);
-    function SaveToString: string;
-    function CreateFromString(text: string): TComponent;
 
-    procedure Assign(source: TPersistent); override;
     function IsEqual(what: TStreamingClass): boolean; virtual;
     function EqualsByAnyOtherName(what: TStreamingClass): boolean; virtual;
 
     function FindOwner: TComponent; //доходит до самого высокого уровня
-
-    function GetFloatProperty(aPath: string): Real;
-    function NameExistsSomewhere(proposedName: string; me: TComponent=nil): boolean; virtual;
-    //есть ли "внутри нас" компонент с таким именем
-    procedure ensureCorrectName(proposedName: string; aowner: TComponent);
-    //убедиться, что при вставке в aowner не возникнет проблемы с нашим именем
-    procedure ensureCorrectNames(aowner: TStreamingClass);
-    //и у всех наших "подчиненных" имена нормальные
-    procedure SetDefaultProperties; //находит default-значения и присваивает их
-
   end;
 
-TStreamingClassClass=class of TStreamingClass;
+EStreamingClassError = class (Exception);
 
-procedure SwapIntegers(var i1,i2: Integer);
-procedure SwapFloats(var f1,f2: Real);
-procedure SwapVariants(var v1,v2: Variant);
+TStreamingClassClass=class of TStreamingClass;
+TSaveToFileProc = procedure (const filename: string) of object;
+TStreamConvertFunc = procedure (input,output: TStream);
 
 procedure SafeSaveToFile(saveProc: TSaveToFileProc; const filename: string);
+procedure RegisterStreamingFormat(formatIdent: Integer; signature: AnsiString; convertFunc: TStreamConvertFunc);
 
 implementation
 
-uses SyncObjs;
-//{$IFDEF UNICODE} ,Winapi.Windows {$ENDIF}
-var fStreamingCriticalSection: TCriticalSection;  //операции в TFiler не являются
-//thread-safe, меняет на ходу DecimalSeparator. Придется по 1 операции за раз
+uses SyncObjs, Contnrs;
+
+var gStreamingCriticalSection: TCriticalSection;
+//TFiler operations are not thread-safe unfortunately
+//it changes DecimalSeparator in the middle of operation
+//It is possible to set DecimalSeparator to '.' at the beginning of your program
+//and not to change it ever, using thread-safe routines with FormatSettings,
+//then you can load/save several StreamingClasses simultaneously
+    gStreamingFormatList: TBucketList;
+
 
 (*
       General procedures
                             *)
+procedure RegisterStreamingFormat(formatIdent: Integer; convertFunc: TStreamConvertFunc);
+begin
+  if gStreamingFormatList.Exists(Pointer(formatIdent)) then
+    Raise EStreamingClassError.CreateFmt('Streaming format %d already registered',[formatIdent]);
+  gStreamingFormatList.Add(Pointer(formatIdent),@convertFunc);
+end;
 
+//if file exists already, we rename it to .BAK at first, if successful,
+//we delete it. In this fashion, any failure during saving document
+//won't destroy it completely, we'll at least have original version
 procedure SafeSaveToFile(saveProc: TSaveToFileProc; const filename: string);
 var backupName: string;
 begin
@@ -79,139 +96,41 @@ begin
     BackupName := ChangeFileExt(FileName, '.BAK');
     if not RenameFile(FileName,BackupName) then
       Raise Exception.Create('couldn''t create BAK file, save procedure canceled');
-    //если попали сюда, то бекап уже есть
+    //OK, backup is ready
     try
       saveProc(filename);
-      DeleteFile(BackUpName);
+      if not DeleteFile(BackUpName) then
+        Raise Exception.Create('couldn''t delete BAK file after saving');
     except
       if FileExists(FileName) then
         DeleteFile(FileName);
-      RenameFile(BackupName,FileName); //если не переименуется, придется уж юзеру смотреть
-      raise;
+      RenameFile(BackupName,FileName);
+      //may be unsuccessful, then user will have to rename BAK file by hand
+      raise;  //whatever exception stopped us from proper saving
     end;
   end
   else
     saveProc(fileName);
 end;
 
-procedure SwapIntegers(var i1,i2: Integer);
-var t: Integer;
-begin
-  t:=i1; i1:=i2; i2:=t;
-end;
-
-procedure SwapFloats(var f1,f2: Real);
-var t: Real;
-begin
-  t:=f1; f1:=f2; f2:=t;
-end;
-
-procedure SwapVariants(var v1,v2: Variant);
-var t: TVarData;
-begin
-//хитрость некая - если в 8 байтах хранятся указатели на другие области данных,
-//мы эти данные не перемещаем, пущай лежат где лежали.
-//если бы мы написали t:=v1; v1:=v2; v2:=t, произошло бы полноценное копирование,
-//удаление и пр. - гораздо медленнее.
-  t:=TVarData(v1); TVarData(v1):=TVarData(v2); TVarData(v2):=t;
-end;
-
+//silly problem with DecimalSeparator
 procedure ThreadSafeWriteComponent(stream: TStream; component: TComponent);
 begin
-  fStreamingCriticalSection.Acquire;
+  gStreamingCriticalSection.Acquire;
   try
     stream.WriteComponent(component);
   finally
-    fStreamingCriticalSection.Release;
+    gStreamingCriticalSection.Release;
   end;
 end;
 
 function ThreadSafeReadComponent(stream: TStream; component: TComponent): TComponent;
 begin
-  fStreamingCriticalSection.Acquire;
+  gStreamingCriticalSection.Acquire;
   try
     Result:=stream.ReadComponent(component);
   finally
-    fStreamingCriticalSection.Release;
-  end;
-end;
-
-
-procedure ObjectTextToCyr(input,output: TStream);
-var c: Char;
-    i: Integer;
-    inside_string: Integer;
-    apostr: Char;
-    getback: string;
-begin
-  apostr:='''';
-  input.Seek(0,soFromBeginning);
-  inside_string:=0; //это когда мы снаружи строки
-  while input.Position<input.Size do begin
-    input.Read(c,1);
-    if inside_string=1 then begin
-      if c=apostr then begin
-        inside_string:=2;  //вышли из строки, но рисовать кавычку не спешим
-//        continue;
-      end
-      else output.write(c,1); //повторяем все символы внутри строки
-    end
-    else begin
-      //мы снаружи, если inside_String=0 значит так и было,
-      //если =2, значит за нами кавычка.
-      if c=apostr then begin
-        if inside_string=0 then output.Write(apostr,1);
-        inside_string:=1; //а если до сих пор 2, значит мы так и не вышли из строки, кавычка не нужна
-      end
-      else if (c<>'#') then begin
-        if inside_string=2 then begin
-          inside_string:=0;
-          output.Write(apostr,1);
-        end;
-        output.Write(c,1);
-        end
-      else begin
-        //самое страшное - мы увидели #
-        input.Read(c,1);
-        if not (c in ['0'..'9']) then begin
-          if inside_string=2 then begin
-            inside_string:=0;
-            getback:=apostr+'#';
-          end
-          else getback:='#';
-          output.Write(getback[1],Length(getback));
-          input.Seek(-1,soFromCurrent);
-        end
-        else begin
-          i:=0;
-          while c in ['0'..'9'] do begin
-            i:=i*10+Ord(c)-Ord('0');
-            input.Read(c,1);
-          end;
-          input.Seek(-1,soFromCurrent);
-          //теперь эту хренотень надо сконвертнуть в кириллицу
-
-          if i<255 then begin
-            if inside_string=2 then begin
-              inside_string:=0;
-              output.Write(apostr,1); //все-таки вышли наружу
-              //вернули апостроф, случайно стыренный ранее
-            end;
-            getback:='#'+IntToStr(i);
-            output.Write(getback[1],Length(getback));
-          end
-          else begin
-            c:=CHR(Byte(i+Ord('А')-1040));
-            if inside_string=0 then begin
-              inside_string:=2;
-              output.Write(apostr,1);
-            end;
-            output.Write(c,1);
-          end;
-        end;
-      end;
-    end;
-
+    gStreamingCriticalSection.Release;
   end;
 end;
 
@@ -234,109 +153,57 @@ begin
       Proc( Components[i] );
 end;
 
-function TStreamingClass.NameExistsSomewhere(proposedName: string; me: TComponent=nil): boolean;
-var i: integer;
-    c: TComponent;
+procedure TStreamingClass.SaveToStream(stream: TStream);
+var
+  BinStream: TMemoryStream;
+  convertProc: TStreamConvertFunc;
 begin
-  c:=FindComponent(proposedName);
-  Result:=Assigned(c) and (c<>me);
-  if not Result then
-    for i:=0 to ComponentCount-1 do begin
-      if Components[i] is TStreamingClass then begin
-        Result:=Result or TStreamingClass(Components[i]).NameExistsSomewhere(proposedName,me);
-        if Result=true then break;
-      end;
-    end;
-end;
-
-procedure TStreamingClass.ensureCorrectName(proposedName: string; aowner: TComponent);
-var FullName: string;
-    i: Integer;
-begin
-  FullName:=proposedName;
-  if assigned(aowner) then begin
-    i:=0;
-//    while aowner.FindComponent(FullName)<>nil do begin
-    while (aowner as TStreamingClass).NameExistsSomewhere(FullName,self) do begin
-      FullName:=proposedName+IntToStr(i);
-      inc(i);
-    end;
-//нужен вариант проверки и компонентах, принадлежащих Owner,
+  if not gStreamingFormatList.Find(Pointer(saveFormat),@convertProc) then
+    Raise Exception.CreateFmt('Streaming format %d not registered',[saveFormat]);
+  if not Assigned(convertProc) then
+    ThreadSafeWriteComponent(stream,Self)
+  else begin
+    BinStream:=TMemoryStream.Create;
+    ThreadSafeWriteComponent(BinStream,self);
+    BinStream.Seek(0, soFromBeginning);
+    convertProc(BinStream,stream);
   end;
-  Name:=FullName;
-end;
-
-procedure TStreamingClass.RecursiveEnsure(our_root,aowner: TStreamingClass);
-var i: Integer;
-    fullName: string;
-begin
-  i:=0;
-  fullName:=Name;
-  while aowner.NameExistsSomewhere(FullName,self) or our_root.NameExistsSomewhere(FullName,self) do begin
-    fullName:=Name+IntToStr(i);
-    inc(i);
-  end;
-  Name:=FullName;
-  //себя переименовали
-  for i:=0 to ComponentCount-1 do
-    if Components[i] is TStreamingClass then
-      TStreamingClass(Components[i]).RecursiveEnsure(our_root,aowner);
-end;
-
-procedure TStreamingClass.ensureCorrectNames(aowner: TStreamingClass);
-begin
-  RecursiveEnsure(self,aowner);
 end;
 
 procedure TstreamingClass.SaveToFile(const filename: string);
 var
-  BinStream: TMemoryStream;
-  BinStreamCyr: TMemoryStream;
   FileStream: TFileStream;
 begin
   FileStream := TFileStream.Create(filename,fmCreate);
   try
-    if saveFormat=fBinary then
-      ThreadSafeWriteComponent(FileStream,Self)
-    else begin
-      BinStream:=TMemoryStream.Create;
-      ThreadSafeWriteComponent(BinStream,self);
-      BinStream.Seek(0, soFromBeginning);
-      if saveFormat=fAscii then ObjectBinaryToText(BinStream,Filestream)
-      else begin
-        BinStreamCyr:=TMemoryStream.Create;
-        ObjectBinaryToText(BinStream, BinStreamCyr);
-        ObjectTextToCyr(BinStreamCyr,FileStream);
-        BinStreamCyr.Free;  //was memory leak
-      end;
-    BinStream.Free
-    end;
+    SaveToStream(FileStream);
   finally
     FileStream.Free;
   end;
-
 end;
 
 function TstreamingClass.SaveToString: string;
-var BinStream: TMemoryStream;
-    StrStream: TStringStream;
-    s: string;
+var StrStream: TStringStream;
 begin
-  BinStream:=TMemoryStream.Create;
+  StrStream:=TStringStream.Create(Result);
   try
-    StrStream:=TStringStream.Create(s);
-    try
-      ThreadSafeWriteComponent(BinStream,self);
-      BinStream.Seek(0,soFromBeginning);
-      ObjectBinaryToText(BinStream,StrStream);
-      StrStream.Seek(0,soFromBeginning);
-      SaveToString:=StrStream.DataString;
-    finally
-      StrStream.Free;
-    end;
+    SaveToStream(StrStream);
   finally
-    BinStream.Free;
+    StrStream.Free;
   end;
+end;
+
+
+constructor TStreamingClass.LoadFromStream(stream: TStream);
+var
+  BinStream: TMemoryStream;
+//  s: array [0..5] of ANSIchar;
+begin
+  Create(nil);
+
+
+
+
 end;
 
 constructor TstreamingClass.LoadFromFile(const filename: string);
@@ -370,6 +237,7 @@ begin
     FileStream.Free;
   end;
 end;
+
 
 constructor TstreamingClass.LoadFromString(const text: string);
 var
@@ -452,27 +320,6 @@ begin
     FileStream.Free;
   end;
 end;
-
-function TstreamingClass.CreateFromString(text: string): TComponent;
-var
-  StrStream: TStringStream;
-  BinStream: TMemoryStream;
-begin
-  BinStream:=TMemoryStream.Create;
-  try
-    StrStream:=TStringStream.Create(text);
-    try
-      ObjectTextToBinary(StrStream,BinStream);
-      BinStream.Seek(0, soFromBeginning);
-      Result:=ThreadSafeReadComponent(BinStream,nil);
-    finally
-      StrStream.Free;
-    end;
-  finally
-    BinStream.Free;
-  end;
-end;
-
 
 procedure TstreamingClass.SaveBinaryToFile(const filename: string);
 var FileStream: TFileStream;
@@ -560,7 +407,7 @@ begin
   else Result:=false;
 end;
 
-constructor TStreamingClass.Clone(source: TStreamingClass;owner: TComponent=nil);
+constructor TStreamingClass.Clone(const source: TStreamingClass; const owner: TComponent=nil);
 begin
   Create(Owner);
   self.Assign(source);
@@ -609,86 +456,26 @@ begin
   end;
 end;
 
-
-procedure TStreamingClass.myGetPropInfo(propPath: string; out Instance: TPersistent; out fPropInfo: PPropInfo);
-var i,j,L: Integer;
-  PropValue: TObject;
-  fPropName: string;
-
+procedure RegisterBinaryStreamingFormat;
+//we've got a lot of headache because classes unit hides signature for binary files
+//we know it's 'TPF0' but who knows, maybe it changes sometimes...
+var w: TWriter;
+    stream: TStream;
+    sig: AnsiString;
 begin
-  i := 1;
-  L := Length(propPath);
-  Instance := FindOwner;
-  while True do
-    begin
-      j := i;
-      while (i <= L) and (PropPath[i] <> '.') do Inc(i);
-      FPropName := Copy(PropPath, j, i - j);
-      if i > l then Break;
-      fPropInfo := GetPropInfo(Instance.ClassInfo, FPropName);
-      if fPropInfo = nil then begin
-        if (Instance is TComponent) then begin
-          Instance:=TComponent(Instance).FindComponent(fPropName) as TPersistent;
-          if Instance=nil then Exception.Create('Property '+FPropName+' not found');
-        end
-        else Raise Exception.Create('Property '+FPropName+' not found');
-      end
-      else begin
-        PropValue := nil;
-        if fPropInfo^.PropType^.Kind = tkClass then
-          PropValue := TObject(GetOrdProp(Instance, fPropInfo));
-        if not (PropValue is TPersistent) then Raise Exception.Create('Wrong property path');
-        Instance := TPersistent(PropValue);
-      end;
-      Inc(I);
-    end;
-    fPropInfo := GetPropInfo(Instance.ClassInfo, FPropName);
+//we won't use try/finally here, absolute sure it'll handle 4 bytes all right
+  stream:=TStringStream.Create(sig);
+  w:=TWriter.Create(stream,4);
+  w.WriteSignature;
+  w.Free;
+  stream.Free;
+  RegisterStreamingFormat(sfBin,nil); //nil corresponds to no transformation at all
 end;
-
-
-function TStreamingClass.GetFloatProperty(aPath: string): Real;
-var instance: TPersistent;
-    fPropInfo: PPropInfo;
-begin
-  myGetPropInfo(aPath,instance,fPropInfo);
-  if fPropInfo.PropType^.Kind<>tkFloat then Raise Exception.Create('error: property is not float number');
-  Result:=GetFloatProp(instance,fPropInfo);
-end;
-
-function TStreamingClass.OwnedBy(Component, Owner: TComponent): Boolean;
-  begin
-    Result := True;
-    while Component <> nil do
-      if Component = Owner then
-        Exit
-      else
-        Component := Component.Owner;
-    Result := False;
-  end;
-
-
-function TStreamingClass.GetComponentValue(Component,LookUpRoot: TComponent): string;
-  begin
-    if Component.Owner = LookupRoot then
-      Result := Component.Name
-    else if Component = LookupRoot then
-      Result := 'Owner'                                                       { Do not translate }
-    else if (Component.Owner <> nil) and (Component.Owner.Name <> '') and
-      (Component.Name <> '') then
-      if OwnedBy(Component.Owner, LookupRoot) then
-        Result := GetComponentValue(Component.Owner,LookUpRoot) + '.' + Component.Name
-      else
-        Result := Component.Owner.Name + '.' + Component.Name
-    else if Component.Name <> '' then
-      Result := Component.Name + '.Owner'                                     { Do not translate }
-    else Result := '';
-  end;
-
-
-
 
 initialization
-  fStreamingCriticalSection:=TCriticalSection.Create;
+  gStreamingCriticalSection:=TCriticalSection.Create;
+  gStreamingFormatList:=TBucketList.Create(bl2);
 finalization
-  FreeAndNil(fStreamingCriticalSection);
+  FreeAndNil(gStreamingFormatList);
+  FreeAndNil(gStreamingCriticalSection);
 end.
